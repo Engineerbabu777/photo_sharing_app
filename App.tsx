3ab25cc17143d99e4eeabd2207e78c3b316d6fd1
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-21 01:08:39 +0500

    chore(project): project has been updated a bit

--- App.tsx
+++
@@ -1,3 +0,0 @@
-// https://www.figma.com/design/Zk6Q4pa5Yne1w5HWmJ8vZD/Whats-App-UI-KIT--Android---Community-?node-id=0-1&p=f&t=XrNVXAFmJoqYIcvC-0
-
-// https://www.figma.com/design/gkV9RgvgIsjdkTZ0ilQB2f/WhatsUp---Chatting-App-UI-Kit--Community-?node-id=2863-93625&p=f&t=bJ980PwjEsAEStSr-0
