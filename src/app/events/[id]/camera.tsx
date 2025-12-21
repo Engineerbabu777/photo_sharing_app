40eb811c6e69fb829a15905234f4a12e866a1561
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-22 01:16:26 +0500

    add(CameraScreen): a couple of states has ben added

--- src/app/events/[id]/camera.tsx
+++ src/app/events/[id]/camera.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { CameraType, useCameraPermissions } from "expo-camera";
 
 type Props = {};
 
 const CameraScreen = (props: Props) => {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+
   return (
     <View>
       <Text>camera</Text>
