26a308766c386c9b74fa1a90e2b146f24b13bae1
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-22 01:11:56 +0500

    add(chnages): a few number of chnage shas been made

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import "../../global.css";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function RootLayout() {
   return (
@@ -12,6 +13,21 @@ export default function RootLayout() {
             title: "Events",
             headerLargeTitle: true,
             headerTransparent: true,
+          }}
+        />
+
+        <Stack.Screen
+          name="events/[id]/camera"
+          options={{
+            title: "Camera",
+            headerBackButtonDisplayMode: "minimal",
+            headerTransparent: true,
+            headerBlurEffect: "dark",
+            headerRight: () => (
+              <Link href="/" className="mr-2 ml-2">
+                <Ionicons name="share-outline" size={24} color="white" />
+              </Link>
+            ),
           }}
         />
       </Stack>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Redirect } from "expo-router";
 
 type Props = {};
 
 const App = (props: Props) => {
-  return <View></View>;
+  return <Redirect href={"/events/[id]/camera"} />;
 };
 
 export default App;
