8ee08c9a3542b9a60e98f636de5ca44e94b78ff2
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-22 11:27:33 +0500

    add(insertAsset): this new service has been added

---
+++ src/services/assets.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/lib/supabase";
+import { TablesInsert } from "@/types/supabase";
+
+export const insertAsset = async (newAsset: TablesInsert<"assets">) => {
+  const { data } = await supabase
+    .from("assets")
+    .insert(newAsset)
+    .select()
+    .single()
+    .throwOnError();
+
+  return data;
+};
