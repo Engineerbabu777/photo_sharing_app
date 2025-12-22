c08db7800db1aa613e0d6e599fc5c98e5555e300
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-22 11:02:33 +0500

    add(AuthProvider): a new provider has been added and wrapped too

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,35 +2,38 @@ import { Link, Stack } from "expo-router";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import "../../global.css";
 import { Ionicons } from "@expo/vector-icons";
+import AuthProvider from "@/providers/AuthProvider";
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={DarkTheme}>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            title: "Events",
-            headerLargeTitle: true,
-            headerTransparent: true,
-          }}
-        />
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Events",
+              headerLargeTitle: true,
+              headerTransparent: true,
+            }}
+          />
 
-        <Stack.Screen
-          name="events/[id]/camera"
-          options={{
-            title: "Camera",
-            headerBackButtonDisplayMode: "minimal",
-            headerTransparent: true,
-            headerBlurEffect: "dark",
-            headerRight: () => (
-              <Link href="/" className="mr-2 ml-2">
-                <Ionicons name="share-outline" size={24} color="white" />
-              </Link>
-            ),
-          }}
-        />
-      </Stack>
+          <Stack.Screen
+            name="events/[id]/camera"
+            options={{
+              title: "Camera",
+              headerBackButtonDisplayMode: "minimal",
+              headerTransparent: true,
+              headerBlurEffect: "dark",
+              headerRight: () => (
+                <Link href="/" className="mr-2 ml-2">
+                  <Ionicons name="share-outline" size={24} color="white" />
+                </Link>
+              ),
+            }}
+          />
+        </Stack>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
