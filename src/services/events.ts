66f9cd94d3e3a3390afec3c7f2fc43737a9d0ca7
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-22 11:23:22 +0500

    add(getEventsForUser): ths new service has been added

--- src/services/events.ts
+++ src/services/events.ts
@@ -4,3 +4,12 @@ export async function getEvents() {
   const { data } = await supabase.from("events").select("*").throwOnError();
   return data;
 }
+
+export async function getEventsForUser(userId: string) {
+  const { data } = await supabase
+    .from("event_memberships")
+    .select("*, events(*, event_memberships(count))")
+    .eq("user_id", userId)
+    .throwOnError();
+  return data.map((event_membership) => event_membership.events);
+}
